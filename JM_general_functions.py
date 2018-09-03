53781294ceccb0f17914e3305683eb3b0d561180
Jaime McCutcheon <jem64@le.ac.uk>
2018-09-03 20:48:04 +0100

    added fx to get usrhome directory

--- JM_general_functions.py
+++ JM_general_functions.py
@@ -14,6 +14,7 @@ import random
 import matplotlib.pyplot as plt
 import xlrd
 import csv
+import os
 
 
 """
@@ -538,3 +539,7 @@ def discrete2continuous(onset, offset=[], nSamples=[], fs=[]):
             outputy[start:stop] = 1
 
     return outputx, outputy
+
+def getuserhome():
+    path = os.environ['USERPROFILE']
+    return path

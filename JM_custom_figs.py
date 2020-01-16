26dadfd855a82f03d2a7fc0cec61a6615ef0c476
Jaime McCutcheon <mccutcheon.james@gmail.com>
2020-01-16 10:43:32 +0100

    Update JM_custom_figs.py
    updated to ensure ticks are removed

--- JM_custom_figs.py
+++ JM_custom_figs.py
@@ -219,8 +219,9 @@ def barscatter(data, transpose = False, unequal=False,
     ax.tick_params(
         axis='x',          # changes apply to the x-axis
         which='both',      # both major and minor ticks are affected
-        bottom='off',      # ticks along the bottom edge are off
-        top='off') # labels along the bottom edge are off
+        bottom=False,      # ticks along the bottom edge are off
+        top=False,
+        labelbottom=False) # labels along the bottom edge are off
 
     if len(xlim) > 0:
         ax.set_xlim(xlim)
